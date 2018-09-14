129047a64ea6bd6186111270301a152e18f570ce
Aman Maharjan <aman.maharjan.tutorials@gmail.com>
2018-09-14 10:16:31 +0545

    Implemented clock face circle drawing code

--- AnalogClock/MainForm.Designer.cs
+++ AnalogClock/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AnalogClock
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(648, 348);
+            this.Name = "MainForm";
+            this.Text = "Drawing Demo";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
---
+++ AnalogClock/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalogClock
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Paint(
+            object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            int r = Math.Min(
+                this.ClientSize.Height / 2 - 10,
+                this.ClientSize.Width / 2 - 10);
+            int d = 2 * r;
+            int x = ClientSize.Width / 2 - r;
+            int y = ClientSize.Height / 2 - r;
+            g.DrawEllipse(Pens.Black, x, y, d, d);
+        }
+
+        private void MainForm_Resize(
+            object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
